ff2ecd3b3858e7fe7cf1ee25fa543bdb83544bbb
dachenlian <cclemon.lian@gmail.com>
2018-08-08 18:22:10 +0800

    enabled memcached cache system

--- web/settings/base.py
+++ web/settings/base.py
@@ -114,6 +114,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
