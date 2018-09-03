4ab118655e5e1676b24b26869539a74a098a991f
amigcamel <amigcamel@gmail.com>
2018-09-04 00:05:43 +0800

    link to all views

--- core/views.py
+++ core/views.py
@@ -1,5 +1,42 @@
+"""Core views."""
 from django.shortcuts import render
 
 
+def index(request):
+    """Index page."""
+    return render(request, 'index.html')
+
+
 def home(request):
+    """Home."""
     return render(request, 'core/home.html')
+
+
+def apidoc(request):
+    """Documentation page for API."""
+    return render(request, 'apidoc.html')
+
+
+def collocation(request):
+    """Collocation page."""
+    return render(request, 'collocation.html')
+
+
+def concordance(request):
+    """Concordance page."""
+    return render(request, 'concordance.html')
+
+
+def segmentation(request):
+    """Segmentation page."""
+    return render(request, 'segmentation.html')
+
+
+def sentipol(request):
+    """Sentipol page."""
+    return render(request, 'sentipol.html')
+
+
+def wordcloud(request):
+    """Wordcloud page."""
+    return render(request, 'wordcloud.html')
