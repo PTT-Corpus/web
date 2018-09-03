ddec8e5b2bc70a4773bda0d51a3ef88e0fd1b42b
amigcamel <amigcamel@gmail.com>
2018-09-04 00:05:34 +0800

    link to all pages

--- web/urls.py
+++ web/urls.py
@@ -1,4 +1,4 @@
-"""web URL Configuration
+"""web URL Configuration.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -12,16 +12,24 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
 """
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views
-from core.views import home
+import core.views
 
 urlpatterns = [
     path(r'admin/', admin.site.urls),
     path(r'login/', views.login, name='login'),
     path(r'logout/', views.logout, name='logout'),
     path(r'auth/', include('social_django.urls', namespace='social')),
-    path(r'', home, name='home')
+    path(r'', core.views.home, name='home'),
+    path(r'index', core.views.index, name='index'),
+    path(r'apidoc', core.views.apidoc, name='apidoc'),
+    path(r'collocation', core.views.collocation, name='collocation'),
+    path(r'concordance', core.views.concordance, name='concordance'),
+    path(r'segmentation', core.views.segmentation, name='segmentation'),
+    path(r'sentipol', core.views.sentipol, name='sentipol'),
+    path(r'wordcloud', core.views.wordcloud, name='wordcloud'),
 ]
