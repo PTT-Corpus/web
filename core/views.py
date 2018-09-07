d981cb0439b9fa5c4de949218c4dafa052187f5a
amigcamel <amigcamel@gmail.com>
2018-09-07 14:05:44 +0800

    complete concordance view

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 """Core views."""
+from datetime import datetime, timedelta
+
 from django.shortcuts import render
-from django.views.generic import TemplateView, FormView
+# from django.views.generic import TemplateView, FormView
 from django.views import View
 
 from .forms import ConcordanceForm
@@ -23,22 +25,32 @@ def collocation(request):
 
 class ConcordanceFormView(View):
     """Concordance page."""
+
     template_name = 'concordance.html'
     form_class = ConcordanceForm
 
     initial = {
-        'word': '帥哥',
-        'page': 0,
+        'word': '好雷',
+        'boards': 'Gossiping',
         'post_type': 0,
         'order': 'desc',
         'sort': 'published',
+        'pos': False,
+        'start': (datetime.now() - timedelta(days=180)),
+        'end': datetime.now(),
     }
 
     def get(self, request, *args, **kwargs):
+        """GET method."""
         form = self.form_class(initial=self.initial)
         return render(request, self.template_name, {'form': form})
 
-
+    def post(self, request, *args, **kwargs):
+        """POST method."""
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            return render(request, 'concordance_result.html', {'data': form.cleaned_data})
+        return render(request, self.template_name, {'form': form})
 
 
 def segmentation(request):
