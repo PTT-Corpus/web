fa26cd81939f829d7499480ace0051e7e7adb558
amigcamel <amigcamel@gmail.com>
2018-09-07 14:04:28 +0800

    complete concordance form

--- core/forms.py
+++ core/forms.py
@@ -1,42 +1,96 @@
-from django import forms
+"""Core app forms."""
 import datetime
+
+from django import forms
+from material import Layout, Row, Column, Fieldset
 
 
 class ConcordanceForm(forms.Form):
-    POST = 0
-    COMMENT = 1
-    POST_TYPE_CHOICES = (
-        (POST, "Posts"),
-        (COMMENT, "Comments"),
+    """Concordance form."""
+
+    YEAR_CHOICES = [
+        str(y)
+        for y
+        in range(2005, datetime.datetime.now().year + 1)
+    ]
+
+    word = forms.CharField(
+        max_length=255,
+        label='',
+    )
+    post_type = forms.CharField(
+        label='',
+        widget=forms.RadioSelect(choices=(
+            (0, "Posts"),
+            (1, "Comments"),
+        ))
+    )
+    boards = forms.CharField(
+        label='',
+        widget=forms.CheckboxSelectMultiple(choices=(
+            ('Gossiping', '八卦板'),
+            ('joke', '就可板'),
+            ('movie', '電影板'),
+        )),
+        required=False,
+    )
+    sort = forms.CharField(
+        label='',
+        widget=forms.RadioSelect(choices=(
+            ('published', 'Publish Time'),
+            ('upvote', 'Likes (推)'),
+            ('downvote', 'Dislikes (噓)'),
+        )),
+    )
+    order = forms.CharField(
+        label='',
+        widget=forms.RadioSelect(choices=(
+            ('desc', 'Descending'),
+            ('asc', 'Ascending')
+        )))
+    start = forms.DateField(
+        label='Start',
+        widget=forms.SelectDateWidget(years=YEAR_CHOICES)
+    )
+    end = forms.DateField(
+        label='End',
+        widget=forms.SelectDateWidget(years=YEAR_CHOICES),
+        initial=datetime.datetime.now
+    )
+    pos = forms.BooleanField(
+        label='Part of Speech',
+        required=False,
+    )
+    window_size = forms.IntegerField(
+        label='',
+        initial=5,
     )
 
-    DESCENDING = 'desc'
-    ASCENDING = 'asc'
-    ORDER_CHOICES = (
-        (DESCENDING, 'Descending'),
-        (ASCENDING, 'Ascending')
+    layout = Layout(
+        Row(
+            Column(
+                Fieldset('Query Word', 'word'),
+                Fieldset('Boards', 'boards'),
+                Fieldset('Window Size', 'window_size'),
+                Fieldset('Post Type', 'post_type'),
+                span_columns=6,
+            ),
+            Column(
+                Fieldset('Part of Speech', 'pos'),
+                Fieldset('Order', 'order'),
+                Fieldset('Sort', 'sort'),
+                Fieldset('Date range', 'start', 'end'),
+                span_columns=6,
+            ),
+        ),
     )
-    YEAR_CHOICES = [str(y) for y in range(2000, datetime.datetime.now().year + 1)]
 
-    word = forms.CharField(max_length=255)
-    page = forms.IntegerField()
-    size = forms.IntegerField(help_text="Number of items to be shown on a page.")
-    post_type = forms.CharField(widget=forms.RadioSelect(choices=POST_TYPE_CHOICES))
-    boards = forms.Textarea()
-    sort = forms.CharField(max_length=255)
-    order = forms.CharField(widget=forms.RadioSelect(choices=ORDER_CHOICES))
-    start_date = forms.DateField(widget=forms.SelectDateWidget(years=YEAR_CHOICES))
-    end_date = forms.DateField(widget=forms.SelectDateWidget(years=YEAR_CHOICES),
-                               initial=datetime.datetime.now)
-
-    # def clean(self):
-    #     cleaned_data = super().clean()
-    #     start_date = cleaned_data.get('start_date')
-    #     print("="*40, type(start_date))
-    #     end_date = cleaned_data.get('end_date')
-    #
-    #     if start_date > end_date:
-    #         raise forms.ValidationError("End date cannot be earlier than start date.")
-    #
-    #     return cleaned_data
-
+    def clean(self):
+        """Process form data."""
+        cleaned_data = super().clean()
+        start = cleaned_data.get('start')
+        end = cleaned_data.get('end')
+        if (start and end) and (start > end):
+            raise forms.ValidationError(
+                "End date cannot be earlier than start date.")
+        return cleaned_data
