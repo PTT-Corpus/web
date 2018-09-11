001137fcaecf098381623f990d5e7236c21ff829
amigcamel <amigcamel@gmail.com>
2018-09-11 15:24:44 +0800

    add STATIC_ROOT and change STATIC_URL

--- web/settings/base.py
+++ web/settings/base.py
@@ -140,10 +140,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_pttweb/'
 STATICFILES_DIRS = [
     os.path.join(os.path.dirname(BASE_DIR), 'static'),
 ]
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_pttweb')
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
