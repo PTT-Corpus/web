113d02bf9bca66c6ed7a845ceffb00c487ce2613
amigcamel <amigcamel@gmail.com>
2018-09-17 15:25:25 +0800

    add local settings

--- web/settings/local.py
+++ web/settings/local.py
@@ -1,1 +1,9 @@
-from .base import *
+"""Django local settings."""
+from .base import *  # noqa: F401, F403
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'pttweb.db',
+    }
+}
