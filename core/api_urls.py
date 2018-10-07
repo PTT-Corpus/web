48bd56f5b8f8f2668d2099007e4bc9a8221b5e3a
dachenlian <cclemon.lian@gmail.com>
2018-10-07 22:27:49 +0800

    added concordance API FBV

--- core/api_urls.py
+++ core/api_urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import re_path
+from django.urls import re_path
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import api_views as views
 
@@ -6,6 +6,7 @@ app_name = 'api'
 
 urlpatterns = [
     re_path(r'^segment/$', views.segment, name='segment'),
+    re_path(r'^concordance/$', views.concordance, name='concordance'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- core/api_views.py
+++ core/api_views.py
@@ -1,10 +1,15 @@
+import os
+import requests
+
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
 from jseg import Jieba
 from ckip import CkipSegmenter
 
 from .serializers import SegmentationSerializer
+from .forms import SegmentationForm, ConcordanceForm
 from .views import SegmentationFormView
 
 _segcom = SegmentationFormView._segcom
@@ -15,10 +20,14 @@ ckip = CkipSegmenter()
 @api_view(['POST'])
 def segment(request, format=None):
     if request.method == 'POST':
-        serializer = SegmentationSerializer(data=request.data)
-        if serializer.is_valid():
-            algo = serializer.validated_data.get('algo')
-            text = serializer.validated_data.get('text')
+        # serializer = SegmentationSerializer(data=request.data)
+        # if serializer.is_valid():
+        #     algo = serializer.validated_data.get('algo')
+        #     text = serializer.validated_data.get('text')
+        form = SegmentationForm(data=request.data)
+        if form.is_valid():
+            algo = form.cleaned_data.get('algo')
+            text = form.cleaned_data.get('text')
             if algo == 'Jseg':
                 output = ' '.join((
                     f'{char}|<span class="pos">{pos}</span>'
@@ -28,10 +37,28 @@ def segment(request, format=None):
             elif algo == 'PyCCS':
                 res = ckip.seg(text)
                 output = ' '.join((f'{char}|<span class="pos">{pos}</span>'
-                           for (char, pos)
-                           in zip(res.tok, res.pos)))
+                                   for (char, pos)
+                                   in zip(res.tok, res.pos)))
             elif algo == 'Segcom':
                 output = _segcom(text)
             return Response({'algo': algo, 'output': output})
 
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def concordance(request, format=None):
+    if request.method == 'POST':
+        form = ConcordanceForm(request.data)
+        if form.is_valid():
+            resp = requests.get(
+            os.environ.get('PTT_ENGINE') + 'query',
+                {k: v for k, v in form.cleaned_data.items() if v},
+            )
+            data = resp.json()
+            return Response({
+                'data': data,
+                'query': request.POST
+            })
+
+    return Response(form.errors.as_json(), status=status.HTTP_400_BAD_REQUEST)
